712e1e6db1bd066af2a341db02210fbb19914b5f
Jan Rundt <jan.rundt@gmail.com>
2022-05-03 15:49:53 +0300

    coverage 100%

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -2,18 +2,47 @@ var { expect } = require("chai");
 var { Shop, Item } = require("../src/gilded_rose.js");
 
 describe("Gilded Rose", function () {
+  it("If no Items are entered into the shop, the items list is empty", function () {
+    const gildedRose = new Shop();
+    expect(gildedRose.items.length).to.equal(0);
+  });
+
   it("If item is given name foo, the name should be foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+  
+  it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC
+      concert or Sulfuras, Hand of Ragnaros, and has a quality value of 0, 
+      update quality will not change the quality value.`, function () {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
 
   it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC
-      concert or Sulfuras, Hand of Ragnaros, and has a quality value above 0, 
+      concert or Sulfuras, Hand of Ragnaros, and has a quality value of 1, 
       update quality will lower quality by one.`, function () {
     const gildedRose = new Shop([new Item("foo", 0, 1)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(0);
+  });
+
+  it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC
+      concert or Sulfuras, Hand of Ragnaros, and has a quality value above 1, 
+      update quality will lower quality by two.`, function () {
+    const gildedRose = new Shop([new Item("foo", 0, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+    });
+
+  it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC
+      concert or Sulfuras, Hand of Ragnaros, and has a quality value above 1 and a
+      selling value above zero, update quality will lower quality by one.`, function () {
+    const gildedRose = new Shop([new Item("foo", 1, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(1);
   });
 
   it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC 
@@ -25,11 +54,20 @@ describe("Gilded Rose", function () {
   });
 
   it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC 
-      concert but it is Sulfuras, Hand of Ragnaros, and has a quality value of 0, 
+      concert but it is Sulfuras, Hand of Ragnaros, and has a quality value above 1, 
       update quality will not lower quality by one.`, function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 2)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(0);
+    expect(items[0].quality).to.equal(2);
+  });
+
+  it(`If the name of the item is not Aged Brie, Backstage passes to a TAFKAL80ETC 
+      concert but it is Sulfuras, Hand of Ragnaros, and has a quality value above 0
+      and a negative sellin-value, update quality will not lower quality by one.
+      `, function () {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(1);
   });
 
   it(`If the name of the item is Aged Brie and has a quality value less than 50, 
@@ -115,6 +153,13 @@ describe("Gilded Rose", function () {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).to.equal(0);
+  });
+
+  it(`If the name of the item is Sulfuras, Hand of Ragnaros, its sellin value will
+      not be reduced by one`, function () {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(1);
   });
 
 
