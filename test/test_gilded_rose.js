86456111775b47b312eeabbaa18885add4fafa43
Jan Rundt <jan.rundt@gmail.com>
2022-05-03 15:08:47 +0300

    93.54% line coverage tests

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -26,9 +26,63 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).to.equal(0);
   });
 
-  it("If the name of the item is Aged Brie and has a quality value less than 50, update quality will add 2 to quality", function (){
+  it("If the name of the item is Aged Brie and has a quality value less than 50, update quality will add 2 to quality", function () {
     const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(2);
-  })
+  });
+
+  it("If the name of the item is Aged Brie and has a quality value at 50 or more, update quality will not change the quality value", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+  });
+
+  it(`If the name of the item is Backstage passes to a TAFKAL80ETC concert and 
+      has a quality value less than 50, and a sellIn of 0, update quality will reduce quality 
+      value to zero`, function () {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+
+  it(`If the name of the item is Backstage passes to a TAFKAL80ETC concert and 
+      has a quality value less than 50, but a sellin value above 0, 
+      update quality will add 3 to quality value`, function () {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(3);
+  });
+
+  it(`If the name of the item is Backstage passes to a TAFKAL80ETC concert and 
+      has a quality value less than 50, but a sellin value of above 5, 
+      update quality will add 2 to quality value`, function () {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 6, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(2);
+  });
+
+  it(`If the name of the item is Backstage passes to a TAFKAL80ETC concert and 
+      has a quality value less than 50, but a sellin value of above 10, 
+      update quality will add 1 to quality value`, function () {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(1);
+  });
+
+  it(`If the name of the item is Backstage passes to a TAFKAL80ETC concert and 
+      has a quality value of 50 or more, and a sellin value of 0,
+      update quality will reset the quality value to zero`, function () {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+
+  it(`If the name of the item is Backstage passes to a TAFKAL80ETC concert and 
+      has a quality value of 50 or more, and a sellin value of 1 or more,
+      update quality will not change the quality value`, function () {
+  const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50)]);
+  const items = gildedRose.updateQuality();
+  expect(items[0].quality).to.equal(50);
+  });
 });
