4825774219807caa8de9c7cbf05b183d36cc3f85
Jan Rundt <jan.rundt@gmail.com>
2022-05-04 18:15:05 +0300

    move up

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -33,10 +33,10 @@ class Shop {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              //if () {
                 this.items[i].quality = this.items[i].quality - 1;
-              }
+              //}
             }
           } else {
             this.items[i].quality = 0;
