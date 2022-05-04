3e35da38998d8525467881f2654b30f845aa5863
Jan Rundt <jan.rundt@gmail.com>
2022-05-04 17:22:10 +0300

    first refactors

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,13 +13,7 @@ class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
+      if (this.items[i].name == "Aged Brie" || this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
@@ -34,6 +28,10 @@ class Shop {
               }
             }
           }
+        }
+      } else {
+        if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          this.items[i].quality = this.items[i].quality - 1;
         }
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
