485ad581a3079a314b3d1a5a4d3dbba4241fd233
Jan Rundt <jan.rundt@gmail.com>
2022-05-05 01:17:21 +0300

    ++ and --

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,35 +15,33 @@ class Shop {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name == "Aged Brie" || this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].quality++;
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].sellIn < 6 && this.items[i].quality < 49) {
               this.items[i].quality = this.items[i].quality + 2;
             } else if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                this.items[i].quality++;
             }
           }
         }
       } else if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros"){
-        this.items[i].quality = this.items[i].quality - 1;
+        this.items[i].quality--;
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.items[i].sellIn--;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert" && this.items[i].quality > 0) {
             if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-              //if () {
-                this.items[i].quality = this.items[i].quality - 1;
-              //}
+                this.items[i].quality--;
             }
           } else {
             this.items[i].quality = 0;
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality++;
           }
         }
       }
