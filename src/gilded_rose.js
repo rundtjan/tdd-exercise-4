1ca185546fb62526a97ddb9f1f24c783731a1cf7
Jan Rundt <jan.rundt@gmail.com>
2022-05-05 14:04:05 +0300

    another switching of if

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -31,17 +31,17 @@ class Shop {
         this.items[i].sellIn--;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert" && this.items[i].quality > 0) {
-            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality--;
-            }
-          } else {
-            this.items[i].quality = 0;
-          }
-        } else {
+        if (this.items[i].name == "Aged Brie") {
           if (this.items[i].quality < 50) {
             this.items[i].quality++;
+          }
+        } else {
+          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+            this.items[i].quality = 0;
+          } else if (this.items[i].quality > 0) {
+            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              this.items[i].quality--;
+            }
           }
         }
       }
