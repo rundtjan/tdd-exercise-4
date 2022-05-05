12e52464ffcafc40f75cb8df8a9b85820fa72899
Jan Rundt <jan.rundt@gmail.com>
2022-05-05 15:27:09 +0300

    checking boundaries in one place

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -19,7 +19,6 @@ class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name == "Sulfuras, Hand of Ragnaros") continue;
-      if (this.items.quality === 0) {this.sellIn--; continue;} 
       this.items[i].sellIn--;
       if (this.items[i].name == "Aged Brie") {
         if (this.sellIn > 0){
@@ -27,30 +26,27 @@ class Shop {
         } else {
           this.items[i].quality += 2;
         }
-        this.checkBoundaries(i);
-        continue;
-      }
-      if (this.items[i].name.includes("Backstage pass")) {
+      } else if (this.items[i].name.includes("Backstage pass")) {
         if (this.items[i].sellIn >= 0){
           if (this.items[i].quality < 50) {
             this.items[i].quality++;
           }
-          if (this.items[i].sellIn < 5 && this.items[i].quality < 49) {
+          if (this.items[i].sellIn < 5) {
             this.items[i].quality += 2;
-          } else if (this.items[i].sellIn < 10 && this.items[i].quality < 50) {
+          } else if (this.items[i].sellIn < 10) {
             this.items[i].quality++;
           }
         } else {
           this.items[i].quality = 0;
         }
-        continue;
-      }
+      } else {
       if (this.items[i].sellIn >= 0) {
         this.items[i].quality--;
       } else {
         this.items[i].quality -= 2; 
       }
-      this.checkBoundaries(i);
+    }
+    this.checkBoundaries(i);
     }
 
     return this.items;
