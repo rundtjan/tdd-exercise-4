374519f450de3af73be896cb161d5bd5d965ae52
Jan Rundt <jan.rundt@gmail.com>
2022-05-05 15:01:12 +0300

    encapsulated backstage passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -26,31 +26,27 @@ class Shop {
         continue;
       }
       if (this.items[i].name.includes("Backstage pass")) {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality++;
-        }
-        if (this.items[i].sellIn < 6 && this.items[i].quality < 49) {
-          this.items[i].quality += 2;
-        } else if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-          this.items[i].quality++;
-        }
-      } else {
-        this.items[i].quality--;
-      }
-
-      this.items[i].sellIn--;
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name == "Aged Brie") {
+        if (this.items[i].sellIn > 0){
           if (this.items[i].quality < 50) {
             this.items[i].quality++;
           }
-        } else {
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            this.items[i].quality = 0;
-          } else if (this.items[i].quality > 0) {
-            this.items[i].quality--;
+          if (this.items[i].sellIn < 6 && this.items[i].quality < 49) {
+            this.items[i].quality += 2;
+          } else if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+            this.items[i].quality++;
           }
+        } else {
+          this.items[i].quality = 0;
         }
+        this.items[i].sellIn--;
+        continue;
+      } //else {
+        this.items[i].quality--;
+      //}
+
+      this.items[i].sellIn--;
+      if (this.items[i].sellIn < 0) {
+        this.items[i].quality--;
       }
       if (this.items[i].quality > 50 ) this.items[i].quality = 50;
       if (this.items[i].quality < 0) this.items[i].quality = 0;
